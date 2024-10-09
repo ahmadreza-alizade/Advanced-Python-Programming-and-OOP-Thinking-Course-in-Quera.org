5597c82054b46d977a0865bb47d2aa2342186115
Ahmadreza Alizade <alizadefardahmadreza@gmail.com>
2024-10-09 17:13:10 +0330

    Update محاسبات سخت.py

--- مروری بر پایتون/محاسبات سخت.py
+++ مروری بر پایتون/محاسبات سخت.py
@@ -1,8 +1,8 @@
-import math
+from math import floor, ceil, radians, pi, sin, tan, atan, gcd, pow
 
 x_deg = int(input())
 
-first_num = math.ceil(math.pow(x_deg, 5/3) + math.tan(math.radians(x_deg)))
-second_num = math.floor(math.pow(math.pi, 2+math.atan(math.pow(math.sin(math.radians(x_deg)), 2))))
+first_num = ceil(pow(x_deg, 5/3) + tan(radians(x_deg)))
+second_num = floor(pow(pi, 2+atan(pow(sin(radians(x_deg)), 2))))
                                     
-print(math.gcd(first_num, second_num))
+print(gcd(first_num, second_num))
