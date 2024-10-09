9b31d46cefb821a50c48fe086654aa20e7e241cb
Ahmadreza Alizade <alizadefardahmadreza@gmail.com>
2024-10-09 10:26:25 +0330

    Create تبدیل نوع متغیرها.py

---
+++ مروری بر پایتون/تبدیل نوع متغیرها.py
@@ -0,0 +1,7 @@
+#برنامه‌ای بنویسید که دو عدد اعشاری مثبت از ورودی بگیرد و در خروجی مقدار جزء صحیح جمع آن‌ها را چاپ کند.
+
+first_num = float(input())
+second_num = float(input())
+
+sum = int(first_num + second_num)
+print(sum)
