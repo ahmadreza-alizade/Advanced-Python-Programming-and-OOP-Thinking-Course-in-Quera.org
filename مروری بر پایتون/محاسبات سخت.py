c727c630a2ff33e8d9741fd28a9bd292ac39fe29
Ahmadreza Alizade <alizadefardahmadreza@gmail.com>
2024-10-09 17:10:12 +0330

    Create محاسبات سخت.py

---
+++ مروری بر پایتون/محاسبات سخت.py
@@ -0,0 +1,9 @@
+import math
+
+x_deg = int(input())
+
+first_num = math.ceil(math.pow(x_deg, 5/3) + math.tan(math.radians(x_deg)))
+second_num = math.floor(math.pow(math.pi, 
+                                    2+math.atan(math.pow(math.sin(math.radians(x_deg)), 2))))
+                                    
+print(math.gcd(first_num, second_num))
