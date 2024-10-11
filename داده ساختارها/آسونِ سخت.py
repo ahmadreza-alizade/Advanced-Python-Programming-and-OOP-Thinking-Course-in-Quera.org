2e6ea5ca64d3e0eb16625e24092930d025c3a8ad
Ahmadreza Alizade <alizadefardahmadreza@gmail.com>
2024-10-11 20:47:48 +0330

    Update آسونِ سخت.py

--- داده ساختارها/آسونِ سخت.py
+++ داده ساختارها/آسونِ سخت.py
@@ -1,2 +1,2 @@
 # TODO: Just in one line!
-print(*sorted([el for index, el in enumerate(list(map(int, input().split()))) if el%6 == 0 and (index+1)%6 == 0]))
+print(*sorted([el for index, el in enumerate(list(map(int, input().split())), start=1) if el%6 == 0 and index%6 == 0]))
