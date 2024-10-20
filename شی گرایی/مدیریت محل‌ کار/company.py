d0c4929fc732044a0253d01bf98488c043ed4286
Ahmadreza Alizade <alizadefardahmadreza@gmail.com>
2024-10-20 11:45:24 +0330

    Create company.py

---
+++ شی گرایی/مدیریت محل‌ کار/company.py
@@ -0,0 +1,17 @@
+# YOUR IMPORTS GO HERE
+from work_place import WorkPlace, Consts
+
+
+class Company(WorkPlace):
+    def __init__(self, name):
+        super().__init__(name)
+        self.expertise="company"
+    
+    def calc_capacity(self):
+        self.capacity = int(self.level)
+
+    def calc_costs(self):
+        return(Consts.BASE_PLACE_COST * self.level)
+
+
+
